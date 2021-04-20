a0dbfb4cc55f1e81ffe441afe02c2bd572d18af3
tiagobarros01 <tbarros1337@hotmail.com>
2021-04-20 11:42:20 -0300

    chore: added icons

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,8 +8,42 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
   body {
-    background: ${(props) => props.theme.colors.gray800};
+    background: ${(props) => props.theme.colors.gray50};
+  }
+
+  body, input, textarea, button {
+    font: 500 1rem Inter, sans-serif;
+    color: ${(props) => props.theme.colors.gray500};
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+    font-family: Lexend, sans-serif;
+    color: ${(props) => props.theme.colors.gray800};
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
 
