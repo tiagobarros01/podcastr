500f7b07a10a86fe6d3c0a0ceae0790d6ba35697
tiagobarros01 <tbarros1337@hotmail.com>
2021-04-20 15:01:29 -0300

    chore: added SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>INdex</h1>
+    <div>
+      <h1>INdex</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/episodes');
+  const data = await res.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
